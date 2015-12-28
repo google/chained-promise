0b64bc5a8838a554f49d74055032a90c25f1a0c0
yiinho <yiinho@google.com>
2015-12-28 15:40:00 -0800

    Replace test promises with Promise#resolve

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,28 +6,25 @@ import ChainedPromise from "../src/index";
 chai.should();
 var assert = chai.assert;
 
-var nullPromise = new Promise((resolver, rejecter) => {});
+var nullPromise = new Promise((resolver, rejecter) => {
+});
 
-describe("ChainedPromise", function() {
-  describe("forEach", function() {
-    it("runs function on each value", function(done) {
+describe("ChainedPromise", function () {
+  describe("forEach", function () {
+    it("runs function on each value", function (done) {
       var dataCollected = [];
-      var thirdPromise = new Promise((resolver, rejecter) => {
-        resolver({
-          data: 3,
-          next: {
-            then: (resolver) => {
-              dataCollected.should.eql([1,2,3]);
-              done();
-            }
+      var thirdPromise = Promise.resolve({
+        data: 3,
+        next: {
+          then: (resolver) => {
+            dataCollected.should.eql([1, 2, 3]);
+            done();
           }
-        });
+        }
       });
-      var secondPromise = new Promise((resolver, rejecter) => {
-        resolver({
-          data: 2,
-          next: thirdPromise
-        });
+      var secondPromise = Promise.resolve({
+        data: 2,
+        next: thirdPromise
       });
       var testChainedPromise = new ChainedPromise((resolver, rejecter) => {
         resolver({

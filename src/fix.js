82270289ac6b45b34bf11c32b7e889db2937a81c
yiinho <yiinho@google.com>
2015-12-30 15:52:01 -0800

    Add fix completion

--- src/fix.js
+++ src/fix.js
@@ -2,7 +2,7 @@
 
 /**
  * Fix operator that continuously resolves next promise returned from the function that consumes
- * resolved previous value.
+ * previously resolved value.
  *
  * ```javascript
  * fix(fn)(promise).catch(errorHandler);
@@ -15,21 +15,56 @@
  *   .catch(errorHandler);
  * ```
  *
- * @param {function(<T>) : Promise} fn
- * @returns {function(Promise.<T>) : Promise.<Promise>}
+ * In addition, the function can complete the chain by calling the complete callback function
+ * supplied as the second argument.
+ *
+ * For example, the following code fragment calculates 5!
+ *
+ * ```javascript
+ * var promiseFactorial = (v, complete) => {
+ *   if (v.i==5) {
+ *     complete(v.f);
+ *   } else {
+ *     return Promise.resolve({i: v.i+1, f: v.f * (v.i + 1)});
+ *   }
+ * };
+ *
+ * fix(promiseFactorial)(Promise.resolve({i:1, f:1})).then(console.log);
+ * ```
+ *
+ * @param {function(<T>, function(<U>)) : Promise} fn
+ * @returns {function(Promise.<T>) : Promise.<U>}
  * @template T
+ * @template U
  */
 var fix = (fn) => (promise) => {
+  /**
+   * Resolve handler to resolve the resulting promise and end the chain.
+   */
+  var finalResolver;
   var rejecter;
   var resolver = (v) => {
     try {
-      var nextPromise = fn(v);
-      nextPromise.then(resolver, rejecter);
+      var completeCalled = false;
+      var completeValue;
+      var complete = function(v) {
+        completeCalled = true;
+        completeValue = v;
+      };
+      var nextPromise = fn(v, complete);
+      if (completeCalled) {
+        finalResolver(completeValue);
+        resolver = null;
+      } else {
+        nextPromise.then(resolver, rejecter);
+      }
+      complete = null;
     } catch(err) {
       rejecter(err);
     }
   };
   return new Promise((resolve, reject) => {
+    finalResolver = resolve;
     rejecter = reject;
     promise.then(resolver, reject);
   });

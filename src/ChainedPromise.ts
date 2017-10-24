7858aa893b457957b3a1c01078b2b1029fb2dc5a
yiinho <yiinho@google.com>
2017-10-24 16:53:15 -0700

    Begin porting to typescript

---
+++ src/ChainedPromise.ts
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2015-2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import fix from './fix';
+
+/**
+ * An extended promise for recurring promises with multiple compositions.
+ *
+ * ```javascript
+ * var chained = ChainedPromise.from(promise);
+ * chained.flatMap(a).flatMap(b).flatMap(c).forEach(fn).catch(onRejected);
+ * ```
+ *
+ * is equivalent to:
+ *
+ * ```javascript
+ * promise.then(a).then(b).then(c).then(fn).then((v) => v.next)
+ *        .then(a).then(b).then(c).then(fn).then((v) => v.next)
+ *        .then(a).then(b).then(c).then(fn).then((v) => v.next)
+ *        ...
+ *        .catch(onRejected);
+ * ```
+ *
+ * `(v) => v.next` function is the default {@link ChainedPromise#next} value
+ * picker. We can supply custom value picker to the {@link
+ * ChainedPromise#constructor} and {@link ChainedPromise#from}.
+ */
+class ChainedPromise<T> extends Promise<T> {
+  private flatMapChain: Array<(v: any) => Promise<any>>;
+
+  /**
+   * Function to construct promise to next item in chain.
+   */
+  public next: (t: T) => Promise<T>;
+
+  /**
+   * Initializes fields common to both {@link ChainedPromise#constructor}
+   * and {@link ChainedPromise#from} code path.
+   */
+  private _initialize() {
+    this.flatMapChain = [];
+  }
+
+  /**
+   * Constructs next {@link ChainedPromise} that carries over settings and
+   * composition properties of the current one.
+   */
+  _nextPromise<U>(v: T): ChainedPromise<U> {
+    const nextPromise = ChainedPromise.from(this.next(v), this.next);
+    nextPromise.flatMapChain = this.flatMapChain;
+    return ((nextPromise as any) as ChainedPromise<U>);
+  }
+
+  /**
+   * @param executor Promise executor
+   * @param next
+   */
+  constructor(executor, next = ChainedPromise.nextFieldPicker<T>('next')) {
+    super(executor);
+    this.next = next;
+    this._initialize();
+  }
+
+  /**
+   * Creates a ChainedPromise that extends given Promise.
+   */
+  static from<T>(
+      innerPromise: Promise<T>,
+      next = ChainedPromise.nextFieldPicker<T>('next')): ChainedPromise<T> {
+    return new ChainedPromise((res, rej) => innerPromise.then(res, rej), next);
+  }
+
+  /**
+   * Returns a function to pick the given attribute.
+   * @param {string} attr Name of the attribute (that will contain the next promise).
+   * @returns {function(T) : Promise.<T>}
+   * @template T
+   */
+  static nextFieldPicker<T>(attr: string): (v: T) => Promise<T> {
+    return (x) => x[attr];
+  }
+
+  /**
+   * Creates `[ChainedPromise, callback, error]` array.
+   *
+   * Calling callback with a value `v` will cause the promise to be resolved
+   * into
+   * `{data: v, next: nextPromise}`, `nextPromise` being another {@link
+   * ChainedPromise} who gets resolved next time `callback` is called.
+   *
+   * Calling `error` function will cause the promise to be rejected.
+   * @returns {Array}
+   * @template T
+   */
+  static createPromiseCallbackPair<U, T = {data: U, next: ChainedPromise<T>}>():
+      [ChainedPromise<T>, (v: U) => void, (error: any) => void] {
+    let resolver;
+    let rejecter;
+    const callback = (v) => {
+      const oldResolver = resolver;
+      const nextPromise = new ChainedPromise((resolve, reject) => {
+        resolver = resolve;
+        rejecter = reject;
+      });
+      oldResolver({data: v, next: nextPromise});
+    };
+    const error = (err) => {
+      rejecter(err);
+    };
+    const promise = new ChainedPromise<T>((resolve, reject) => {
+      resolver = resolve;
+      rejecter = reject;
+    });
+    return [promise, callback, error];
+  }
+
+  /**
+   * Applies the given function on all values in the chain, until the {@link
+   * ChainedPromise#next} value returns an object with {@link
+   * ChainedPromise.DONE} symbol.
+   */
+  forEach<V>(fn: (v: T) => void): Promise<V> {
+    return fix<T, V>((v: T, complete) => {
+      fn(v);
+      const nextPromise = this.next(v);
+      if (nextPromise[ChainedPromise.DONE] !== undefined) {
+        return complete(nextPromise[ChainedPromise.DONE]);
+      } else {
+        return this._nextPromise(v);
+      }
+    })(this);
+  }
+
+  /**
+   * Stacks up flat map operation to be performed in this promise. See {@link
+   * ChainedPromise} for examples.
+   */
+  flatMap<U>(fn: (v: T) => Promise<U>): ChainedPromise<U> {
+    this.flatMapChain.push(fn);
+    return ((this as any) as ChainedPromise<U>);
+  }
+
+  /**
+   * Non-async equivalent of {@link ChainedPromise#flatMap}.
+   */
+  map<U>(fn: (T) => U): ChainedPromise<U> {
+    this.flatMap((v) => new ChainedPromise((res) => res(fn(v))));
+    return ((this as any) as ChainedPromise<U>);
+  }
+
+  /**
+   * Overrides Promise.then to compose with extra functions. See {@link
+   * ChainedPromise} for the specifics of available compositions.
+   */
+  then<TResult1 = T, TResult2 = never>(
+      onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>)|null|
+      undefined,
+      onRejected?: (result: any) => TResult2 |
+          PromiseLike<TResult2>): Promise<TResult1|TResult2> {
+    if (!onFulfilled) {
+      // Skip processing in case of Promise.catch call.
+      // TODO: fix type.
+      return super.then(onFulfilled as any, onRejected as any) as any;
+    }
+    // Branch out no-op special case, since "super" in ES6 is not a first-class
+    // citizen.
+    if (this.flatMapChain.length === 0) {
+      // TODO: fix type.
+      return super.then(onFulfilled as any, onRejected as any) as any;
+    } else {
+      const firstFlatMapped = super.then(this.flatMapChain[0]);
+      let flatMapped = this.flatMapChain.slice(1).reduce(
+          (x, y) => x.then(y), firstFlatMapped);
+      return flatMapped.then(onFulfilled, onRejected);
+    }
+  }
+
+  /**
+   * Flat-maps current promise chain to resolve into successive accumulation of
+   * values, from given accumulator. Accumulator should pass on next promise to
+   * the accumulated value.
+   * @param fn Accumulator that takes previous accumulation and current value,
+   * and calculate next accumulation.
+   * @param initial Initial accumulated value to start with.
+   */
+  accumulate<U>(fn: (U, T) => Promise<U>, initial: U): ChainedPromise<U> {
+    let accumulated = initial;
+    this.flatMap((v) => fn(accumulated, v)).map((acc) => {
+      accumulated = acc;
+      return acc;
+    });
+    return ((this as any) as ChainedPromise<U>);
+  }
+
+  /**
+   * Takes a join spec and flatMaps current ChainedPromise accordingly. A join
+   * spec is recursively defined as follows:
+   *
+   *   * If the spec is a function taking a value and returning a promise, then
+   * the join operation evaluates the function with current value and replaces
+   * the value with the resulting promise.
+   *
+   *   * If the spec is an array of a spec, then the current value is assumed to
+   * be an array, and each element in the current value is mapped to the inner
+   * spec.
+   *
+   *   * If the spec is an object with keys to specs, then the field of the
+   * current value with each key is replaced with the result of each join
+   * operations with the inner spec.
+   * @param {(function(T): (Promise.<U>) | Array | Object)} spec
+   * @returns {ChainedPromise.<V>}
+   * @template T
+   * @template U
+   * @template V
+   */
+  join<U, V>(spec: ((t: T) => Promise<U>)|Array<Object>|
+             Object): ChainedPromise<V> {
+    this.flatMap((v) => {
+      function pickAndJoin(curSpec, curValue) {
+        if (typeof curSpec === 'function') {
+          return curSpec(curValue);
+        }
+        if (curSpec instanceof Array) {
+          // TODO(yiinho): more thorough error handling.
+          return Promise.all(curValue.map((x) => pickAndJoin(curSpec[0], x)));
+        }
+        if (curSpec instanceof Object) {
+          return Promise
+              .all(Object.keys(curSpec).map(
+                  (key) => pickAndJoin(curSpec[key], curValue[key])
+                               .then((joinResult) => {
+                                 const result = {};
+                                 result[key] = joinResult;
+                                 return result;
+                               })))
+              .then((joinedValues) => {
+                joinedValues.forEach((join) => Object.assign(curValue, join));
+                return curValue;
+              });
+        }
+        throw new TypeError(
+            'Specification not recognized: ' + JSON.stringify(spec));
+      }
+      return pickAndJoin(spec, v);
+    });
+    return this as any as ChainedPromise<V>;
+  }
+
+  /**
+   * Collects results (including the final "done" value) into an array.
+   * @param fn Mapper function to be applied to each data points (except
+   * the final "done" value) before collecting into the result array.
+   */
+  collect<U, V>(fn: (t: T) => (U | V) = (x) => (x as any as U)) {
+    const collected: Array<U|V> = [];
+    return this
+        .forEach((v) => {
+          collected.push(fn(v));
+        })
+        .then((done: V) => {
+          collected.push(done);
+          return collected;
+        });
+  }
+
+  /**
+   * Symbol to indicate the end of promise chains. Having
+   * `{[ChainedPromise.DONE]: <some value>}` as a next value will indicate the
+   * end of the chain, and will cause fixed promises such as
+   * {@link ChainedPromise#forEach} to resolve to the given value.
+   */
+  static DONE = Symbol('ChainedPromise.DONE');
+}
+
+export default ChainedPromise;
---
+++ test/ChainedPromise.spec.ts
@@ -0,0 +1,390 @@
+/*
+ * Copyright 2015-2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import ChainedPromise from '../src/index';
+
+describe('ChainedPromise', function() {
+  describe('createPromiseCallbackPair', function() {
+    it('creates ChainedPromise and callback functions', function(done) {
+      const dataCollected: Array<number> = [];
+      const [promise, callback] =
+          ChainedPromise.createPromiseCallbackPair<number>();
+      callback(1);
+      callback(2);
+      callback(3);
+      promise
+          .forEach((v) => {
+            dataCollected.push(v.data);
+            if (dataCollected.length === 3) {
+              expect(dataCollected).toEqual([1, 2, 3]);
+              done();
+            }
+          })
+          .catch((err) => console.error(err.stack, err));
+    });
+  });
+  it('creates error function which rejectes the promise', function(done) {
+    const dataCollected: Array<number> = [];
+    const [promise, callback, error] =
+        ChainedPromise.createPromiseCallbackPair<number>();
+    callback(1);
+    callback(2);
+    error('Error!');
+    promise
+        .forEach(({data}) => {
+          dataCollected.push(data);
+        })
+        .catch((err) => {
+          expect(dataCollected).toEqual([1, 2]);
+          expect(err).toEqual('Error!');
+          done();
+        });
+  });
+  describe('forEach', function() {
+    it('runs function on each value', function(done) {
+      const dataCollected: Array<number> = [];
+      const thirdPromise =
+          Promise.resolve({data: 3, next: {[ChainedPromise.DONE]: 'done'}});
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise =
+          new ChainedPromise<{data: number}>((resolver) => {
+            resolver({data: 1, next: secondPromise});
+          });
+      testChainedPromise
+          .forEach((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([1, 2, 3]);
+            done();
+          })
+          .catch(done);
+    });
+  });
+  it('bubbles up error', function(done) {
+    const dataCollected: Array<number> = [];
+    const testChainedPromise = ChainedPromise.from(Promise.resolve({
+      data: 1,
+      next: Promise.resolve({data: 2, next: Promise.reject('Error!')})
+    }));
+    testChainedPromise
+        .forEach(({data}) => {
+          dataCollected.push(data);
+        })
+        .catch((err) => {
+          expect(dataCollected).toEqual([1, 2]);
+          expect(err).toEqual('Error!');
+          done();
+        });
+  });
+  describe('flatMap', function() {
+    it('composes flat map functions', function(done) {
+      const addOnePromise = function(v) {
+        return Promise.resolve(Object.assign(v, {data: v.data + 1}));
+      };
+      const doublePromise = function(v) {
+        return Promise.resolve(Object.assign(v, {data: v.data * 2}));
+      };
+      const dataCollected: Array<number> = [];
+      const thirdPromise = Promise.resolve({
+        data: 3,
+        next: {
+          then: (resolver) => {
+            if (!resolver) {
+              // Skip catch call.
+              return;
+            }
+            expect(dataCollected).toEqual([4, 6, 8]);
+            done();
+          }
+        }
+      });
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise = new ChainedPromise((resolver) => {
+        resolver({data: 1, next: secondPromise});
+      });
+      testChainedPromise.flatMap(addOnePromise)
+          .flatMap(doublePromise)
+          .forEach((v) => {
+            dataCollected.push(v.data);
+          })
+          .catch((err) => console.error(err.stack, err));
+    });
+  });
+  describe('map', function() {
+    it('composes map functions', function(done) {
+      const addOneFunction = (v) => Object.assign(v, {data: v.data + 1});
+      const doubleFunction = (v) => Object.assign(v, {data: v.data * 2});
+      const dataCollected: Array<number> = [];
+      const thirdPromise = Promise.resolve({
+        data: 3,
+        next: {
+          then: (resolver) => {
+            if (!resolver) {
+              // Skip catch call.
+              return;
+            }
+            expect(dataCollected).toEqual([4, 6, 8]);
+            done();
+          }
+        }
+      });
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise = new ChainedPromise((resolver) => {
+        resolver({data: 1, next: secondPromise});
+      });
+      testChainedPromise.map(addOneFunction)
+          .map(doubleFunction)
+          .forEach((v) => {
+            dataCollected.push(v.data);
+          })
+          .catch((err) => console.error(err.stack, err));
+    });
+  });
+  describe('then', function() {
+    it('bypasses composition when registering reject handler only',
+       function(done) {
+         const testChainedPromise = new ChainedPromise<number>((resolver) => {
+           resolver(1);
+         });
+         testChainedPromise.flatMap((v) => Promise.resolve(v * 2));
+         testChainedPromise.then(undefined, console.error);
+         testChainedPromise.then((v) => {
+           expect(v).toEqual(2);  // i.e. flatMapped function should have been
+                                  // invoked only once.
+           done();
+         });
+       });
+  });
+  describe('accumulate', function() {
+    it('should accumulate values', function(done) {
+      const dataCollected: Array<number> = [];
+      const thirdPromise =
+          Promise.resolve({data: 3, next: {[ChainedPromise.DONE]: 'done'}});
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise = new ChainedPromise((resolver) => {
+        resolver({data: 1, next: secondPromise});
+      });
+      testChainedPromise
+          .accumulate(
+              (prevSum, newValue) => {
+                return Promise.resolve(Object.assign(
+                    newValue, {data: prevSum.data + newValue.data}));
+              },
+              {data: 0})
+          .forEach((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([1, 3, 6]);
+            done();
+          })
+          .catch(done);
+    });
+  });
+  describe('join', function() {
+    it('extracts field', function(done) {
+      type raw = {name: string, ref: number};
+      type joined = {name: string,
+                     ref: string} const dataCollected: Array<joined> = [];
+      const secondPromise = new ChainedPromise<{data: raw}>((resolver) => {
+        resolver({
+          data: {name: 'Test Person 2', ref: 43},
+          next: {[ChainedPromise.DONE]: 'done'}
+        });
+      });
+      const testChainedPromise = new ChainedPromise<{data: raw}>((resolver) => {
+        resolver({data: {name: 'Test Person', ref: 42}, next: secondPromise});
+      });
+      testChainedPromise
+          .join<string, {data: joined}>(
+              {data: {ref: (v) => new Promise((res) => res('Reference ' + v))}})
+          .forEach<string>((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([
+              {name: 'Test Person', ref: 'Reference 42'},
+              {name: 'Test Person 2', ref: 'Reference 43'}
+            ]);
+            done();
+          })
+          .catch(done);
+    });
+    it('supports multiple fields', function(done) {
+      type raw = {name: string, book: {ref: number}, car: {ref: number}};
+      type joined = {name: string, book: {ref: string}, car: {ref: string}};
+      const dataCollected: Array<joined> = [];
+      const testChainedPromise = new ChainedPromise<{data: raw}>((resolver) => {
+        resolver({
+          data: {name: 'Test Person', book: {ref: 42}, car: {ref: 43}},
+          next: {[ChainedPromise.DONE]: 'done'}
+        });
+      });
+      testChainedPromise
+          .join<string, {data: joined}>({
+            data: {
+              book: {
+                ref: (v) => new Promise((res) => res('Book reference ' + v))
+              },
+              car: {
+                ref: (v) => new Promise((res) => res('Car reference ' + v))
+              }
+            }
+          })
+          .forEach<string>((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([{
+              name: 'Test Person',
+              book: {ref: 'Book reference 42'},
+              car: {ref: 'Car reference 43'}
+            }]);
+            done();
+          })
+          .catch(done);
+    });
+    it('maps array of values', function(done) {
+      type raw = {name: string, book: {ref: number}, car: {ref: number}};
+      type joined = {name: string, book: {ref: string}, car: {ref: string}};
+      const dataCollected: Array<joined> = [];
+      const secondPromise = new ChainedPromise<{data: raw}>((resolver) => {
+        resolver({
+          data: {name: 'Test Person 2', ref: [44, 45]},
+          next: {[ChainedPromise.DONE]: 'done'}
+        });
+      });
+      const testChainedPromise = new ChainedPromise((resolver) => {
+        resolver(
+            {data: {name: 'Test Person', ref: [42, 43]}, next: secondPromise});
+      });
+      testChainedPromise
+          .join<string, {data: joined}>({
+            data: {ref: [(v) => new Promise((res) => res('Reference ' + v))]}
+          })
+          .forEach((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([
+              {name: 'Test Person', ref: ['Reference 42', 'Reference 43']},
+              {name: 'Test Person 2', ref: ['Reference 44', 'Reference 45']}
+            ]);
+            done();
+          })
+          .catch(done);
+    });
+    it('supports object specification inside an array', function(done) {
+      type raw = {name: string, book: [{ref: number}], car: [{ref: number}]};
+      type joined = {name: string, book: [{ref: string}], car: [{ref: string}]};
+      const dataCollected: Array<joined> = [];
+      const secondPromise = new ChainedPromise((resolver) => {
+        resolver({
+          data: {name: 'Test Person 2', refs: [{book: 44}, {book: 45}]},
+          next: {[ChainedPromise.DONE]: 'done'}
+        });
+      });
+      const testChainedPromise = new ChainedPromise<{data: raw}>((resolver) => {
+        resolver({
+          data: {name: 'Test Person', refs: [{book: 42}, {book: 43}]},
+          next: secondPromise
+        });
+      });
+      testChainedPromise
+          .join<string, {data: joined}>({
+            data: {
+              refs: [
+                {book: (v) => new Promise((res) => res('Reference ' + v))}
+              ]
+            }
+          })
+          .forEach<string>((v) => {
+            dataCollected.push(v.data);
+          })
+          .then((v) => {
+            expect(v).toEqual('done');
+            expect(dataCollected).toEqual([
+              {
+                name: 'Test Person',
+                refs: [{book: 'Reference 42'}, {book: 'Reference 43'}]
+              },
+              {
+                name: 'Test Person 2',
+                refs: [{book: 'Reference 44'}, {book: 'Reference 45'}]
+              }
+            ]);
+            done();
+          })
+          .catch(done);
+    });
+    it('throws when the given spec is illegal', function(done) {
+      const testChainedPromise = new ChainedPromise((resolver) => {
+        resolver({
+          data: {name: 'Test Person', ref: 42},
+          next: {[ChainedPromise.DONE]: 'done'}
+        });
+      });
+      testChainedPromise.join({data: {name: 'hello'}})
+          .then((v) => {
+            done(v);
+          })
+          .catch(() => {
+            done();
+          });
+    });
+  });
+  describe('collect', function() {
+    it('collects result in an array', function(done) {
+      const thirdPromise =
+          Promise.resolve({data: 3, next: {[ChainedPromise.DONE]: 'done'}});
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise =
+          new ChainedPromise<{data: number}>((resolver) => {
+            resolver({data: 1, next: secondPromise});
+          });
+      testChainedPromise.collect<{data: number}, string>()
+          .then((result) => {
+            expect((result[0] as {data: number}).data).toEqual(1);
+            expect((result[1] as {data: number}).data).toEqual(2);
+            expect((result[2] as {data: number}).data).toEqual(3);
+            expect(result[3]).toEqual('done');
+            expect(result.length).toEqual(4);
+            done();
+          })
+          .catch(done);
+    });
+    it('transforms data before collecting into an array', function(done) {
+      const thirdPromise =
+          Promise.resolve({data: 3, next: {[ChainedPromise.DONE]: 'done'}});
+      const secondPromise = Promise.resolve({data: 2, next: thirdPromise});
+      const testChainedPromise =
+          new ChainedPromise<{data: number}>((resolver) => {
+            resolver({data: 1, next: secondPromise});
+          });
+      testChainedPromise.collect<number, string>((v) => v.data)
+          .then((result) => {
+            expect(result).toEqual([1, 2, 3, 'done']);
+            done();
+          })
+          .catch(done);
+    });
+  });
+});
